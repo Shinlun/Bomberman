570357ebd6f4c13ce5bdef4623f418c8ec45eaab
Ronan <ronanlet@gmail.com>
2011-05-20 11:40:00 +0200

    ADD: méthode d'encodage de données client-serveur en JSON

--- Client/src/bomberman/model/Client.java
+++ Client/src/bomberman/model/Client.java
@@ -3,7 +3,12 @@ package bomberman.model;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.Socket;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 public class Client {
 
@@ -31,5 +36,12 @@ public class Client {
         socket = new Socket(host, port);
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public Object encodeData(Object data) throws Exception {
+        StringWriter jsonOut = new StringWriter();
+        JSONValue.writeJSONString(data, jsonOut);
+
+        return jsonOut.toString();
     }
 }
