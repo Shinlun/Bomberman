ffa87d09cd6d23f71deb3a274a0b861a952b1238
Ronan <ronanlet@gmail.com>
2011-05-20 12:19:55 +0200

    ADD: protrocol thread et début du server main

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,11 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
+    final static int port = 18000;
+    private static int threadNb = 0;
+
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(port);
+        while (true) {
+            System.out.println("Attente de joueur via le nem vapeur " + port + "...");
+            new ProtocolThread(serverSocket.accept()).start();
+            threadNb++;
+        }
     }
 
 }
