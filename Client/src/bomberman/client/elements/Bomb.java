78eceb46c7388d916afa4440c974acc57f2f29b6
Ronan <ronanlet@gmail.com>
2011-05-26 18:14:25 +0200

    Add: sprite bomb et modificaton de la classe Bomb en conséquence

--- Client/src/bomberman/client/elements/Bomb.java
+++ Client/src/bomberman/client/elements/Bomb.java
@@ -1,6 +1,10 @@
 package bomberman.client.elements;
 
+import bomberman.client.gui.Window;
 
 public class Bomb extends Element {
 
+    public Bomb() {
+        this.image = Window.getInstance().getToolkit().getImage("images/sprite_bomb.png");
+    }
 }
--- Client/src/bomberman/client/model/Client.java
+++ Client/src/bomberman/client/model/Client.java
@@ -1,7 +1,6 @@
 package bomberman.client.model;
 
 import bomberman.client.controller.Game;
-import bomberman.client.elements.Player;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
